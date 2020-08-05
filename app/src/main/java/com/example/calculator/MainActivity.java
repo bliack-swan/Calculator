a755483dd1b1c3d193418f39b2ee13cf0c5cb4c8
bliack-swan <61846791+bliack-swan@users.noreply.github.com>
2020-08-05 16:50:17 +0300

    Wrote button for numeral and comma, number's orientation, delete last symbol

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,71 +8,120 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView result;
-    TextView cal_symbol;
-    char symbol;
-    EditText input_place;
-    double num1 ;
-    double num2 ;
+    private TextView number_result;
+    private String text;
+    private char symbol;
+    private double num1;
+    private double num2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        result = findViewById(R.id.result_number);
-        cal_symbol = findViewById(R.id.cal_symbol);
-        symbol = '=';
-        input_place = findViewById(R.id.input_number);
+        number_result = findViewById(R.id.result_number);
+        num1 = 0;
+        text = "";
     }
-    public void onPlus(View view){
-        cal_symbol.setText(getResources().getText(R.string.cal_plus));
+
+    // Operation button
+    public void onPlus(View view) {
         symbol = '+';
     }
-    public void onMinus(View view){
-        cal_symbol.setText(getResources().getText(R.string.cal_minus));
+
+    public void onMinus(View view) {
         symbol = '-';
     }
-    public void onStar(View view){
-        cal_symbol.setText(getResources().getText(R.string.cal_star));
+
+    public void onStar(View view) {
         symbol = 'x';
     }
-    public void onSlash(View view){
-        cal_symbol.setText(getResources().getText(R.string.cal_slash));
+
+    public void onSlash(View view) {
         symbol = '/';
     }
-    public void onRoof(View view){
-        cal_symbol.setText(getResources().getText(R.string.cal_roof));
+
+    public void onRoof(View view) {
         symbol = '^';
     }
-    public void onEqual(View view){
-        num1 = Double.parseDouble(result.getText().toString());
-        num2 = Double.parseDouble(input_place.getText().toString());
-        switch (symbol){
-                case '=':
-                    num1 = num2;
-                    break;
-                case '+':
-                    num1 += num2;
-                    break;
-                case '-':
-                    num1 -= num2;
-                    break;
-                case 'x':
-                    num1 *= num2;
-                    break;
-                case '/':
-                    num1 /= num2;
-                    break;
-                case '^':
-                    num1 = Math.pow(num1,num2);
-                    break;
-            }
-            result.setText(String.valueOf(num1));
+
+    public void onEqual(View view) {
+        symbol = '=';
+    }
+
+    public void onReset(View view) {
+        number_result.setText("0");
+    }
+    public void onDel(View view){
+        text = number_result.getText().toString();
+        number_result.setText(text.substring(0,text.length()-1));
+    }
+    // Calculation button
+    public void onOne(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("1");
         }
-    public void onReset(View view){
-        num1 = 0;
-        cal_symbol.setText(getResources().getText(R.string.cal_equal));
-        result.setText(String.valueOf(num1));
+        else{number_result.append("1");}
     }
+    public void onTwo(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("2");
+        }
+        else{number_result.append("2");}
     }
+    public void onThree(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("3");
+        }
+        else{number_result.append("3");}
+    }
+    public void onFour(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("4");
+        }
+        else{number_result.append("4");}
+    }
+    public void onFive(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("5");
+        }
+        else{number_result.append("5");}
+    }
+    public void onSix(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("6");
+        }
+        else{number_result.append("6");}
+    }
+    public void onSeven(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("7");
+        }
+        else{number_result.append("7");}
+    }
+    public void onEight(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("8");
+        }
+        else{number_result.append("8");}
+    }
+    public void onNine(View view){
+        if (number_result.getText().toString().equals("0")){
+            number_result.setText("9");
+        }
+        else{number_result.append("9");}
+    }
+    public void onZero(View view) {
+        if (!number_result.getText().toString().equals("0")) {
+            number_result.append("0");
+        }
+    }
+    public void onComma(View view) {
+        text = number_result.getText().toString();
+        if(!text.contains(",")){
+            number_result.append(",");
+        }
+
+    }
+
+}
